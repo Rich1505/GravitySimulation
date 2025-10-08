bf42c40c10f37ed1ca4f88977d0ff11140ac704d
Rich1505 <rpistillo43@gmail.com>
2025-10-08 08:21:44 +0200

    improved readability

--- UserInterface.h
+++ UserInterface.h
@@ -35,8 +35,8 @@ struct Button {
 		Rectangle temp = rec;
 		temp.y = temp.y + offsetY;
 		//DrawRectangle(rec.x,rec.y + offsetY,rec.width,rec.height, Color{ 0,0,0,100 });
-		DrawRectangleRounded(temp, 0.2f, 0, Color{ 0,0,0,100 });
-		DrawCircle((int)(rec.x + rec.width / 2), (int)(rec.y + rec.height / 2 + offsetY), rec.width / 2 - 5, color);
+		DrawRectangleRounded(temp, 0.2f, 60, Color{ 0,0,0,100 });
+		DrawCircle(rec.x + rec.width / 2, rec.y + rec.height / 2 + offsetY, rec.width/2 - 5, color);
 	}
 };
 
