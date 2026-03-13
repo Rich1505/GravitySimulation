3887cb9b04a08e70443465742b4ba729658c096f
Riccardo Pistillo <rpistillo43@gmail.com>
2026-03-13 10:01:48 +0100

    cmake implementation

--- src/UserInterface.h
+++ src/UserInterface.h
@@ -1,14 +1,15 @@
 #pragma once
-#include<vector>
-#include"Body.h"
-#include<string>
-#include<iostream>
-#include<raygui.h>
+#include <vector>
+#include "Body.h"
+#include <string>
+#include <iostream>
+#include <raygui.h>
 
 constexpr int UI_WIDTH = 400;
 
-struct MassSelector {
-	
+struct MassSelector
+{
+
 	static constexpr int minValue = 200;
 	static constexpr int maxValue = 1000000000;
 	static constexpr int width = 300;
@@ -19,7 +20,7 @@ struct MassSelector {
 	int prevKeyPressed;
 
 	MassSelector()
-		:rec{ 0,0,0,0}, value(200),prevValue(200), text(""),prevKeyPressed(0) { }
+		: rec{0, 0, 0, 0}, value(200), prevValue(200), text(""), prevKeyPressed(0) {}
 
 	void draw(int offsetY)
 	{
@@ -42,7 +43,7 @@ struct MassSelector {
 		if (k != KEY_BACKSPACE && k != 0)
 		{
 			char number[11];
-			strcpy(number,_itoa(value, number, 10));
+			snprintf(number, sizeof(number), "%d", value);
 
 			int i = (int)strlen(number);
 
@@ -56,7 +57,8 @@ struct MassSelector {
 	}
 };
 
-struct Slider {
+struct Slider
+{
 	Rectangle range;
 	Rectangle selector;
 	int maxValue;
@@ -64,18 +66,18 @@ struct Slider {
 	int value;
 	int width = 800;
 
-	Slider(Rectangle range, Rectangle selector,int maxValue, int minValue)
-		:range(range), selector(selector), maxValue(maxValue), minValue(minValue)
+	Slider(Rectangle range, Rectangle selector, int maxValue, int minValue)
+		: range(range), selector(selector), maxValue(maxValue), minValue(minValue)
 	{
 		value = (int)Remap((float)range.x + range.width / 2, (float)range.x, (float)range.x + range.width, (float)minValue, (float)maxValue);
 		this->selector.y = selector.y - (selector.height - range.height) / 2;
 	}
 
 	Slider(int width)
-		:range{ 0,0,0,0 }, selector{ 0,0,0,0 }, maxValue(0), minValue(0), value(0),width(width) {}
+		: range{0, 0, 0, 0}, selector{0, 0, 0, 0}, maxValue(0), minValue(0), value(0), width(width) {}
 
 	Slider()
-		:range{ 0,0,0,0 }, selector{ 0,0,0,0 }, maxValue(0), minValue(0), value(0), width(0) {}
+		: range{0, 0, 0, 0}, selector{0, 0, 0, 0}, maxValue(0), minValue(0), value(0), width(0) {}
 
 	bool checkCollision(Vector2 mousePos)
 	{
@@ -86,14 +88,14 @@ struct Slider {
 	{
 		if (mousePos.x < range.x + range.width && mousePos.x > range.x)
 		{
-			
-			selector.x = mousePos.x - selector.width/2;
-			value = (int)Remap((float)selector.x+selector.width/2.0f, (float)range.x, (float)range.x + range.width, (float)minValue, (float)maxValue);
-			if (abs(selector.x + selector.width / 2 - range.x) <=2)
+
+			selector.x = mousePos.x - selector.width / 2;
+			value = (int)Remap((float)selector.x + selector.width / 2.0f, (float)range.x, (float)range.x + range.width, (float)minValue, (float)maxValue);
+			if (abs(selector.x + selector.width / 2 - range.x) <= 2)
 			{
 				value = minValue;
 			}
-			else if(abs(selector.x + selector.width / 2 - (range.x + range.width)) <= 2)
+			else if (abs(selector.x + selector.width / 2 - (range.x + range.width)) <= 2)
 			{
 				value = maxValue;
 			}
@@ -107,17 +109,18 @@ struct Slider {
 	{
 		Rectangle temp = range;
 		temp.y = temp.y + offsetY;
-		DrawRectangleRec(temp, Color{ 0,0,0,100 });
+		DrawRectangleRec(temp, Color{0, 0, 0, 100});
 		temp = selector;
 		temp.y = temp.y + offsetY;
-		DrawRectangleRec(temp, Color{ 200,200,200,200 });
+		DrawRectangleRec(temp, Color{200, 200, 200, 200});
 	}
 };
 
-struct Button {
+struct Button
+{
 	Rectangle rec;
 	Color color;
-	
+
 	static constexpr int width = 100;
 	static constexpr int height = 100;
 	static constexpr int spaceX = 50;
@@ -125,17 +128,17 @@ struct Button {
 	static constexpr int startingY = 200;
 
 	Button(Color color)
-		:rec{0, 0, width, height}, color{color.r, color.g, color.b, 200} {}
+		: rec{0, 0, width, height}, color{color.r, color.g, color.b, 200} {}
 
-	Button() 
-		:rec{ 0,0,0,0 }, color{ 0,0,0 } {}
+	Button()
+		: rec{0, 0, 0, 0}, color{0, 0, 0} {}
 
 	void draw(int offsetY)
 	{
 		Rectangle temp = rec;
 		temp.y = temp.y + offsetY;
-		DrawRectangleRounded(temp, 0.2f, 10, Color{ 0,0,0,100 });
-		DrawCircle((int)(rec.x + rec.width / 2), (int)(rec.y + rec.height / 2 + offsetY), rec.width/2.0f - 5.0f, color);
+		DrawRectangleRounded(temp, 0.2f, 10, Color{0, 0, 0, 100});
+		DrawCircle((int)(rec.x + rec.width / 2), (int)(rec.y + rec.height / 2 + offsetY), rec.width / 2.0f - 5.0f, color);
 	}
 
 	void drawSelected(int offsetY)
@@ -146,7 +149,8 @@ struct Button {
 	}
 };
 
-struct UIText {
+struct UIText
+{
 	static constexpr int fontSize = 20;
 
 	std::string text;
@@ -154,11 +158,11 @@ struct UIText {
 	int x;
 	int y;
 
-	UIText(const char text[], int y) 
-		:text(text), width(MeasureText(text, fontSize)), x((GetScreenWidth() - UI_WIDTH) + (UI_WIDTH - width) / 2), y(y){}
+	UIText(const char text[], int y)
+		: text(text), width(MeasureText(text, fontSize)), x((GetScreenWidth() - UI_WIDTH) + (UI_WIDTH - width) / 2), y(y) {}
 
-	UIText() 
-		:text(""), width(0), x(0), y(0){}
+	UIText()
+		: text(""), width(0), x(0), y(0) {}
 
 	void draw(int offsetY)
 	{
@@ -170,7 +174,7 @@ class UserInterface
 {
 private:
 	Color color;
-	//mouse position in the previous frame translated in world coordinates
+	// mouse position in the previous frame translated in world coordinates
 	Vector2 prevMousePos;
 	UIText texts[4];
 	Button buttons[6];
@@ -183,7 +187,7 @@ public:
 	int startingX;
 	int offsetY;
 	static constexpr int width = UI_WIDTH;
-	void draw(bool gamePaused,char* coordinates,int gameSpeed,int defaultGameSpeed);
+	void draw(bool gamePaused, char *coordinates, int gameSpeed, int defaultGameSpeed);
 	void drawText();
 	void drawButtons();
 	void drawGameSpeed(int gameSpeed, int defaultGameSpeed);
@@ -196,8 +200,8 @@ public:
 	{
 		selectedButton = -1;
 		startingX = 0;
-		color = Color{ 100,100,100,200};
-		prevMousePos = { 0.0f,0.0f };
+		color = Color{100, 100, 100, 200};
+		prevMousePos = {0.0f, 0.0f};
 		offsetY = 0;
 	}
 };
